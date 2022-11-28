31d77b4a31536c0e065cf758dd9f5245cdec0121
SARAVANAKUMAR <saravana1802kumar@gmail.com>
2022-11-28 22:54:11 +0530

    commit

--- src/component/home/Social.jsx
+++ src/component/home/Social.jsx
@@ -4,16 +4,16 @@ import './home.css'
 const Social = () => {
   return (
     <div className="home_social">
-      <a href="Javascript(void)" className="home_social-icon" target='_blank'>
-          <i className="uil uil-instagram insta icon_size"></i>
+      <a href="https://www.instagram.com/sk._.logan/" className="home_social-icon" target='_blank' >
+        <i className="uil uil-instagram insta icon_size"></i>
       </a>
-      
-      <a href="Javascript(void)" className="home_social-icon" target='_blank'>
-      <i className="uil uil-github github icon_size"></i>
+
+      <a href="https://github.com/Saravanakumar1802" className="home_social-icon" target='_blank' >
+        <i className="uil uil-github github icon_size"></i>
       </a>
-      
-      <a href="Javascript(void)" className="home_social-icon" target='_blank'>
-      <i className="uil uil-linkedin linked_in icon_size"></i>
+
+      <a href="Javascript(void)" className="home_social-icon" target='_blank' >
+        <i className="uil uil-linkedin linked_in icon_size"></i>
       </a>
 
     </div>
