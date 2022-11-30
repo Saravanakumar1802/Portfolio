4c5b0d7d2edd156ebf187269b934766df17ee37e
SARAVANAKUMAR <saravana1802kumar@gmail.com>
2022-11-30 10:30:39 +0530

    cmmit

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className="about">
                 <h1 className='abt_me'>About Me</h1>
 
-                <div className="about_content"><p>My name is <span>Saravanakumar</span> and I have recently completed a Bachelor of Engineering, majoring in Computing Science at Anna University. I have a passion in <span>Frond-End developing</span>.I am
+                <div className="about_content"><p>My name is <span>Saravanakumar</span> and I have recently completed a Bachelor of Engineering, majoring in Computer Science at Anna University. I have a passion in <span>Frond-End developing</span>.I am
                     looking to join forces to grow my skill set while contributing to the positive outcome of making people
                     "richer, smarter, and happier."
                 </p>
