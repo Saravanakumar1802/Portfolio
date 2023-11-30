5bda340acd0de9a74dc7addee43e8c78a6dcf670
Saravanakumar1802 <saravana1802kumar@gmail.com>
2023-12-01 00:09:33 +0530

    project  added

--- src/component/projects/ProjectData.jsx
+++ src/component/projects/ProjectData.jsx
@@ -7,7 +7,7 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 
 //ProjectCard Pics
 import todoListProjectPic from "../../assets/todolist-mini-project.png"
-import chatApp from "../../assets/chat app.png"
+// import chatApp from "../../assets/chat app.png"
 // import pokemonApiProjectPic from "../../assets/pokemon-api.png"
 import gamingPageProjectPic from "../../assets/gaming-page.png"
 import portfoliopic from "../../assets/my_portfolio.png"
@@ -19,6 +19,7 @@ import QuoteGenerator from '../../assets/Quote.png'
 import youtube from '../../assets/YouTube.png'
 import linktree from '../../assets/linkTree.png'
 import clock from '../../assets/digitalClock.png'
+import bookApp from '../../assets/openBook.jpeg'
 
 const ProjectData = () => {
     let card = [{
@@ -26,7 +27,16 @@ const ProjectData = () => {
         projectSummary: 'This YouTube_Clone created using reactJs, nodejs, expressjs, MongoDb.',
         image: youtube,
         github: 'https://github.com/Saravanakumar1802/Youtube-Clone-Frontend',
+        githubServer: 'https://github.com/Saravanakumar1802/Youtube-Clone-Backend',
         netify: 'https://youtube-clone-saravana.netlify.app/',
+    },
+    {
+        projectTitle: 'OpenBook App',
+        projectSummary: 'I developed a Book Site. Combining MongoDB, Express.js, React, and Node.js, and firebase Auth.',
+        image: bookApp,
+        github: 'https://github.com/Saravanakumar1802/OpenBook-App-Client',
+        githubServer: 'https://github.com/Saravanakumar1802/OpenBook-App-Server',
+        netify: 'https://openbook-app-saravana.netlify.app/',
     },
     {
         projectTitle: 'Review Rockerz',
@@ -36,8 +46,25 @@ const ProjectData = () => {
         netify: 'https://movie-site-saravana.netlify.app/',
     },
     {
+
+        projectTitle: 'Money_Manager',
+        projectSummary: 'Money Manager App is a web-based financial management application built using the MERN stack',
+        image: moneyManager,
+        github: 'https://github.com/Saravanakumar1802/Money-Manager-frontend',
+        githubServer: 'https://github.com/Saravanakumar1802/Money-Manager-backend',
+        netify: 'https://money-manager-app-saravana.netlify.app/',
+    },
+    {
+        projectTitle: 'TODO App',
+        projectSummary: 'MERN stack ToDo App It allows users to manage their tasks efficiently',
+        image: todoApp,
+        github: 'https://github.com/Saravanakumar1802/Fullstack-ToDo-App-Frontend',
+        githubServer: '',
+        netify: 'https://mern-todo-app-saravana.netlify.app/',
+    },
+    {
         projectTitle: 'My Portfolio',
-        projectSummary: 'This portfolio created using react Js',
+        projectSummary: 'A portfolio built with React.js. It is a dynamic and responsive.I additionally used emailJs and tostify etc..',
         image: portfoliopic,
         github: 'https://github.com/Saravanakumar1802/Portfolio',
         netify: 'https://portfolio-saravana.netlify.app/',
@@ -50,28 +77,20 @@ const ProjectData = () => {
         netify: 'https://weather-api-saravana.netlify.app/',
     },
     {
-        projectTitle: 'TODO App',
-        projectSummary: 'MERN stack ToDo App',
-        image: todoApp,
-        github: 'https://github.com/Saravanakumar1802/Fullstack-ToDo-App-Frontend',
-        netify: 'https://mern-todo-app-saravana.netlify.app/',
-    },
-    {
-
         projectTitle: 'Link-Tree',
-        projectSummary: 'I Created linkTree uing HTML5, Css, Js',
+        projectSummary: 'I Created linkTree uing HTML5, Css, Js. Its a single-page website presenting a collection of links.',
         image: linktree,
         github: 'https://github.com/Saravanakumar1802/Link-tree',
         netify: 'https://link-tree-saravana.netlify.app/',
     },
 
-    {
-        projectTitle: 'LetsChat App',
-        projectSummary: 'Created Chat App using react Js, react hooks, ChatEngine and FirebaseAuth',
-        image: chatApp,
-        github: 'https://github.com/Saravanakumar1802/Chat_App',
-        netify: 'https://chat-app-saravana.netlify.app/',
-    },
+    // {
+    //     projectTitle: 'LetsChat App',
+    //     projectSummary: 'Created Chat App using react Js, react hooks, ChatEngine and FirebaseAuth',
+    //     image: chatApp,
+    //     github: 'https://github.com/Saravanakumar1802/Chat_App',
+    //     netify: 'https://chat-app-saravana.netlify.app/',
+    // },
     // {
     //     projectTitle: 'Pokemon Api',
     //     projectSummary: 'Pokemon API using async/await, fetch and HTML, Css',
@@ -86,18 +105,10 @@ const ProjectData = () => {
         github: 'https://github.com/Saravanakumar1802/Random_Quote_Generator',
         netify: 'https://quote-generator-api-saravana.netlify.app/',
     },
-    {
-
-        projectTitle: 'Money_Manager',
-        projectSummary: 'MERN stack Money manager App',
-        image: moneyManager,
-        github: 'https://github.com/Saravanakumar1802/Money-Manager-frontend',
-        netify: 'https://money-manager-app-saravana.netlify.app/',
-    },
     ]
+
     let card2 = [
         {
-
             projectTitle: 'Digital Clock',
             projectSummary: 'I Created Digital Clock using HTML, Css, Javascript',
             image: clock,
--- src/component/projects/ProjectsCard.jsx
+++ src/component/projects/ProjectsCard.jsx
@@ -14,12 +14,25 @@ const ProjectsCard = ({ data }) => {
                     <a href={data.netify} target="_blank" rel="noreferrer" className="project-link">Demo
                         <i className="project_preview uil uil-arrow-up-right"></i>
                     </a></Button> &nbsp;
-
-                <Button variant="primary" size="sm" className='projects-link-btn'>
-                    <a href={data.github} target="_blank" rel="noreferrer" className="project-link">Code
+                {data.githubServer ? <Button variant="primary" size="sm" className='projects-link-btn'>
+                    <a href={data.github} target="_blank" rel="noreferrer" className="project-link">Front
                         <i className="project_github uil uil-github"></i>
                     </a>
-                </Button>
+                </Button> :
+
+                    <Button variant="primary" size="sm" className='projects-link-btn'>
+                        <a href={data.github} target="_blank" rel="noreferrer" className="project-link">Code
+                            <i className="project_github uil uil-github"></i>
+                        </a>
+                    </Button>
+                }&nbsp;&nbsp;
+
+                {data.githubServer ? <Button variant="primary" size="sm" className='projects-link-btn'>
+                    <a href={data.githubServer} target="_blank" rel="noreferrer" className="project-link">Back
+                        <i className="project_github uil uil-github"></i>
+                    </a>
+                </Button> : null}
+
             </div>
         </div>
     )
